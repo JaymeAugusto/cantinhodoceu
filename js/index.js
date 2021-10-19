d36360f3188bf23cd3b9a955e17294c0ab6f3c98
Jayme Augusto <ja.guto01@gmail.com>
2021-10-19 13:28:21 -0300

    implementando menu Dropdown

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
-tamanhoTelaW = window.innerWidth
-tamanhoTelaH = window.innerHeight
+const abreMenu = document.querySelector(".btn-menu")
+const menu = document.querySelector(".menu")
+const fecha = document.querySelector(".btn-close")
+
+abreMenu.addEventListener("click",function(){
+    menu.classList.add("menuopen") 
+})
+
+fecha.addEventListener("click",function(){
+    menu.classList.remove("menuopen") 
+})
