67607f96770b918e508a5dcc67437be59a75ec66
Jayme Augusto <ja.guto01@gmail.com>
2021-10-21 15:52:48 -0300

    Final dos trabalhos do dia 21/10

--- js/index.js
+++ js/index.js
@@ -9,3 +9,25 @@ abreMenu.addEventListener("click",function(){
 fecha.addEventListener("click",function(){
     menu.classList.remove("menuopen") 
 })
+
+const abrefotos = document.querySelector(".arrowdown")
+const fechafotos = document.querySelector(".arrowup")
+const divfotos = document.querySelector(".mostpic")
+
+abrefotos.addEventListener("click",function(){
+    divfotos.classList.remove("mostpic")
+    divfotos.classList.add("mostpictrue")
+    abrefotos.classList.remove("viewtrue")
+    abrefotos.classList.add("viewfalse")
+    fechafotos.classList.add("viewtrue")
+    fechafotos.classList.remove("viewfalse")
+})
+
+fechafotos.addEventListener("click",function(){
+    divfotos.classList.add("mostpic")
+    divfotos.classList.remove("mostpictrue")
+    abrefotos.classList.add("viewtrue")
+    abrefotos.classList.remove("viewfalse")
+    fechafotos.classList.remove("viewtrue")
+    fechafotos.classList.add("viewfalse")
+})
